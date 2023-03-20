2b0c7e7d84db988ac5c054cb1958900917a5148b
kimziwu <jiwoo.kim@dream-academy.io>
2023-03-20 17:19:16 +0900

    test

--- src/DreamAcademyLending.sol
+++ src/DreamAcademyLending.sol
@@ -1,25 +1,63 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-
-contract DreamOracle {
-	address public operator;
-	mapping(address=>uint256) prices;
+import './IPriceOracle.sol';
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 
-	constructor() {
-		operator = msg.sender;
+/**
+이자율 : 0.1%
+LTV : 50%
+liquidation threshold : 75%
+ */
+
+contract DreamAcademyLending {
+
+	ERC20 public _usdc;
+	uint public _totalUsdc;
+	uint public _initialTime;
+
+
+	constructor(IPriceOracle oracle, address usdc) {
+		_usdc=ERC20(usdc);
+		_initialTime=block.number; //1-1
 	}
 
 
-	function getPrice(address token) external view returns (uint256) {
-	   require(prices[token] != 0, "the price cannot be zero");
-	   return prices[token];
+	function initializeLendingProtocol(address usdc) public payable {
+		_usdc.transferFrom(msg.sender, address(this), msg.value);
+		_totalUsdc+=msg.value;
+		_initialTime+=block.number;
+	}
+
+	function deposit(address tokenAddress, uint256 amount) external payable {
+
 	}
 
 
-	function setPrice(address token, uint256 price) external {
-	   require(msg.sender == operator, "only operator can set the price");
-	   prices[token] = price;
+	function borrow(address tokenAddress, uint256 amount) external {
+
 	}
+
+
+	function repay(address tokenAddress, uint256 amount) external {
+
+	}
+
+
+	function liquidate(address user, address tokenAddress, uint256 amount) external {
+
+	}
+
+
+	function withdraw(address tokenAddress, uint256 amount) external {
+
+	}
+
+	function getAccruedSupplyAmount(address usdc) public returns (uint256) {
+
+	}
+
+
+	
 }
